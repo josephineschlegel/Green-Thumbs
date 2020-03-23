a04ce9719cf09afac6459186f5158ae152884b00
Shaireen <jankowskam401@gmail.com>
2020-03-23 17:43:16 +0100

    first animation

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".open").click(function() {
+    $("#triangle-down").addClass("hidden");
+    $("#triangle-up").addClass("hidden");
+    $("content").addClass("curtains");
+    $("#triangle-up").delay(700).fadeOut();
+    $("#title").fadeOut(300);
+    $(".titleHide").fadeOut(300);
+    $(".textHide").hide();
+    $("content").delay(700).fadeIn(400);
+    $("#triangle-down").delay(700).fadeOut();
+
+  });
+
+
+});
+
